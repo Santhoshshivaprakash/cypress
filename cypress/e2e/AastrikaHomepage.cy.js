1078555a476218788ef5f81262e1163a46fe3835
Santhoshhs21 <118436502+Santhoshhs21@users.noreply.github.com>
2023-02-01 15:16:09 +0530

    First commit

---
+++ cypress/e2e/AastrikaHomepage.cy.js
@@ -0,0 +1,11 @@
+describe('Aastrka Home page', () => {
+  before(() => {
+    })
+    beforeEach(() => {
+    
+})
+it.only('click command', () => {
+cy.visit('https://sphere.aastrika.org/app/create-account')
+//cy.get('div.form-control ng-pristine ng-invalid ng-touched>button').click()
+})
+})
